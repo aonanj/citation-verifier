b5a921180856309f4b8217b7b5c503fa2e4ea65e
aonanj <admin@spurly.io>
2025-10-06 14:24:17 -0700

    chore: add additional allowed origins for CORS in FastAPI middleware

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ app.add_middleware(
         "https://127.0.0.1:3000",
         "https://localhost:5174",
         "https://127.0.0.1:5174",
+        "https://citation-verifier.onrender.com",
+        "https://citation-verifier.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
